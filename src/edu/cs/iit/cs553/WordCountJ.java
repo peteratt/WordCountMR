8cc561f69d0ffdd943e1698bcdd40f6565987a7e
Pedro Alvarez-Tabio <paltat@gmail.com>
2012-03-08 18:49:10 -0600

    script

--- src/edu/cs/iit/cs553/WordCountJ.java
+++ src/edu/cs/iit/cs553/WordCountJ.java
@@ -1,18 +1,15 @@
 package edu.cs.iit.cs553;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 
+/**
+ * TextCount - cuenta caracteres, palabras y frases en textos en castellano.
+ * 
+ * @author Pedro çlvarez-Tab’o
+ *
+ */
 public class WordCountJ {
-	/// JESUS
-	
-	// Pedro
-	/**
-	 * @param args
-	 */
+
 	public static void main (String args[]) throws Exception {
 		
 		if (2 < args.length)
@@ -22,10 +19,9 @@ public class WordCountJ {
 		if (args.length > 0)
 			i = new BufferedReader(new FileReader(args[0]));
 		
-		String output = "result" + System.currentTimeMillis() + ".txt";
-		
 		PrintWriter o = new PrintWriter(System.out);
-		o = new PrintWriter(new FileWriter(args[1]));
+		if (args.length == 2)
+			o = new PrintWriter(new FileWriter(args[1]));
 		
 		String line, text = "";
 		while ((line = i.readLine()) != null) {
@@ -35,38 +31,38 @@ public class WordCountJ {
 		if (text.matches("[^a-zA-Z]*\\Z"))
 			text = ""; // elimina ficheros sin palabras
 		
-		// Versiï¿½n 1.0: Reconoce palabras sin guion del espaï¿½ol
+		// Versi—n 1.0: Reconoce palabras sin guion del espa–ol
 		
-		// Versiï¿½n 1.1: Reconoce palabras con guion del espaï¿½ol (no salto de lï¿½nea)
-		// "[^a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]*[a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]+-?[a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]*[^a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]*"
-		// NOTA: la palabra 9Hola56 la reconoce como palabra, no deberï¿½a
+		// Versi—n 1.1: Reconoce palabras con guion del espa–ol (no salto de l’nea)
+		// "[^a-zA-Z–„‡-œŸ†]*[a-zA-Z–„‡-œŸ†]+-?[a-zA-Z–„‡-œŸ†]*[^a-zA-Z–„‡-œŸ†]*"
+		// NOTA: la palabra 9Hola56 la reconoce como palabra, no deber’a
 		
-		// Versiï¿½n 1.2: Reconoce nï¿½meros con decimal y signo
-		// "[^a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]*(([a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]+-?[\na-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½])|([0-9]+?,?[0-9]*))*[^a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]*"
+		// Versi—n 1.2: Reconoce nœmeros con decimal y signo
+		// "[^a-zA-Z–„‡-œŸ†]*(([a-zA-Z–„‡-œŸ†]+-?[\na-zA-Z–„‡-œŸ†])|([0-9]+?,?[0-9]*))*[^a-zA-Z–„‡-œŸ†]*"
 		
-		// Versiï¿½n 1.3: Reconoce ya con salto de lï¿½nea
-		// "[^a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]*(([a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]+-?[\\\na-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½])|([0-9]+?,?[0-9]*))*[^a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]*"
+		// Versi—n 1.3: Reconoce ya con salto de l’nea
+		// "[^a-zA-Z–„‡-œŸ†]*(([a-zA-Z–„‡-œŸ†]+-?[\\\na-zA-Z–„‡-œŸ†])|([0-9]+?,?[0-9]*))*[^a-zA-Z–„‡-œŸ†]*"
 		String regexPalabras = 
-			"[^a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]*(([a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]+-?[\\\na-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½])|([0-9]+?,?[0-9]*))*[^a-zA-Zï¿½ï¿½ï¿½-ï¿½ï¿½ï¿½]*";
+			"[^a-zA-Z–„‡-œŸ†]*(([a-zA-Z–„‡-œŸ†]+-?[\\\na-zA-Z–„‡-œŸ†])|([0-9]+?,?[0-9]*))*[^a-zA-Z–„‡-œŸ†]*";
 				
 		// No me funciona la regex que aparece en las transparencias: "[^a-zA-Z]*([a-zA-Z]+[^a-zA-Z]+)+"
-		// pero hago split y sï¿½ que va
+		// pero hago split y s’ que va
 		String regexFrases = "[.?!]+";
 				
-		text = "ï¿½lvarez-Tabï¿½o Cigï¿½eï¿½a CIGï¿½Eï¿½A cam-\npana. -98517,5325."; // Prueba sin cargar desde archivo
+		text = "çlvarez-Tab’o CigŸe–a CIG†E„A cam-\npana. -98517,5325."; // Prueba sin cargar desde archivo
 
-		text = "ï¿½cigï¿½eï¿½as CIGï¿½Eï¿½AS! tienen   \n    34662,1123 metros de envergadura";
+		text = "ÀcigŸe–as CIG†E„AS! tienen   \n    34662,1123 metros de envergadura";
 		
 		System.out.println(text);
 		
 		int numPalabras = text.replaceAll(regexPalabras, "w").length();
 				
-		// Aquï¿½, en vez de replaceAll, con split y mi regex va bien
+		// Aqu’, en vez de replaceAll, con split y mi regex va bien
 		int numFrases = text.split(regexFrases).length;
 		
 		int numCaracteres = text.length();
 		
-		// ï¿½Por quï¿½ si cargo desde archivo me cuenta siempre una palabra de mï¿½s?
+		// ÀPor quŽ si cargo desde archivo me cuenta siempre una palabra de m‡s?
 		
 		o.println("Caracteres: " + numCaracteres + ", " + 
 				"Palabras: " + numPalabras + ", " + "Frases: " + numFrases);
