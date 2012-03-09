0136ec469a0ee407a30c03747d3df5468579011d
Pedro Alvarez-Tabio <paltat@gmail.com>
2012-03-09 14:10:43 -0600

    3/9 initial commit

--- src/edu/cs/iit/cs553/WordCountJ.java
+++ src/edu/cs/iit/cs553/WordCountJ.java
@@ -1,17 +1,43 @@
 package edu.cs.iit.cs553;
 
 /**
- * TextCount - cuenta caracteres, palabras y frases en textos en castellano.
- * 
- * @author Pedro �lvarez-Tab�o
+ * WordCount Java version main
  *
  */
 public class WordCountJ {
-	/**
-	 * @param args
-	 */
+	
+	private static final int N_FILES = 0;
+	private static int busyThreads = 0;
+	
 	public static void main (String args[]) throws Exception {
 		
+		int nThreads = Integer.parseInt(args[0]);
+		
+		for (int i = 0; i < N_FILES; i++) {
+			
+			while (getBusyThreads() == nThreads) {
+				Thread.wait();
+			}
+			String filename = "../input_" + i + ".txt";
+			WordCountThread wc = new WordCountThread(filename);
+			Thread tWc = new Thread(wc);
+			tWc.start();
+			incrementBusyThreads();
+		}
+		
 	}
+	
+	private static synchronized int getBusyThreads() {
+		return busyThreads;
+	}
+	
+	private static synchronized void incrementBusyThreads() {
+		busyThreads++;
+	}
+	
+	public static synchronized void decrementBusyThreads() {
+		busyThreads--;
+	}
+	
 }
 
