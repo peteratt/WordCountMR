56d94c0f06abee3010d12ec8941738407fdb1391
Pedro Alvarez-Tabio <paltat@gmail.com>
2012-03-10 17:53:48 -0600

    v0.2 - added Hadoop WordCount

---
+++ hadoopexec.sh
@@ -0,0 +1,16 @@
+if [[ -f wordcount.jar ]]; then
+	rm wordcount.jar
+fi
+
+if [[ -f hadoopexec ]]; then
+	rm -rf hadoopexec
+fi
+
+if [[ -f output ]]; then
+	rm -rf output
+fi
+
+cp src/edu/cs/iit/cs553/hadoop/WordCountMR.java .
+mkdir hadoopexec
+javac -classpath $HADOOP_HOME/hadoop-core-0.20.203.0.jar:$HADOOP_HOME/lib/commons-cli-1.2.jar -d hadoopexec WordCountMR.java && jar -cvf wordcount.jar -C hadoopexec/ .
+hadoop jar wordcount.jar edu.cs.iit.cs553.hadoop.WordCountMR
---
+++ src/edu/cs/iit/cs553/hadoop/WordCountMR.java
@@ -0,0 +1,100 @@
+package edu.cs.iit.cs553.hadoop;
+
+import java.io.IOException;
+import java.lang.InterruptedException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCountMR {
+	/**
+	 * The map class of WordCount.
+	 */
+	public static class WordCountMapper extends
+			Mapper<Object, Text, Text, IntWritable> {
+
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+		
+		/*
+		 * This "homemade" regex handles words like:
+		 * 
+		 * six-year-old They're you--I
+		 */
+		String regexWords = "([a-zA-Z]+-{0,2})*([a-zA-Z]+'?)*[a-zA-Z]+";
+		Pattern wordCountPattern;
+		Matcher m;
+
+		public void map(Object key, Text value, Context context)
+				throws IOException, InterruptedException {
+			
+			// Modify this for the regex
+			wordCountPattern = Pattern.compile(regexWords);
+			m = wordCountPattern.matcher(value.toString());
+
+			// Iterate over words found in the line
+			while (m.find()) {
+				// IMPORTANT: lower case the words so we don't have problems
+				word.set(m.group().toLowerCase());
+				context.write(word, one);
+			}
+		}
+	}
+
+	/**
+	 * The reducer class of WordCount
+	 */
+	public static class WordCountReducer extends
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterable<IntWritable> values,
+				Context context) throws IOException, InterruptedException {
+			int sum = 0;
+			for (IntWritable value : values) {
+				sum += value.get();
+			}
+			context.write(key, new IntWritable(sum));
+		}
+	}
+
+	/**
+	 * The main entry point.
+	 */
+	public static void main(String[] args) throws Exception {
+		// Initial timestamp
+		long initialTime = System.currentTimeMillis();
+				
+		Configuration conf = new Configuration();
+		
+		Job job = new Job(conf, "Hadoop WordCount");
+		job.setJarByClass(WordCountMR.class);
+		
+		// Sets
+		job.setMapperClass(WordCountMapper.class);
+		job.setReducerClass(WordCountReducer.class);
+		
+		// Sets output keys and values
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(job, new Path("./input"));
+		FileOutputFormat.setOutputPath(job, new Path("./output"));
+		
+		boolean completed = job.waitForCompletion(true);
+		// Final timestamp
+		long finalTime = System.currentTimeMillis();
+		
+		long totalTime = finalTime - initialTime;
+		System.out.println("Word Count Hadoop time: " + totalTime + " ms");
+		
+		System.exit(completed ? 0 : 1);
+	}
+}
